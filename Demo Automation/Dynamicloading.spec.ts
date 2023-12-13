3d4e99103b33c418329418a9b170572e238923a2
vigneshfz <130372830+vigneshfz@users.noreply.github.com>
2023-12-13 10:39:05 +0530

    Update Dynamicloading.spec.ts

--- Demo Automation/Dynamicloading.spec.ts
+++ Demo Automation/Dynamicloading.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://practice.expandtesting.com/');
-  await page.getByRole('link', { name: 'Dynamic Controls' }).click();
+  await page.getByRole('link', { name: 'Dynamic Loading' }).click();
   await page.getByRole('link', { name: 'Example 1: Element on page that is hidden' }).click();
   await page.getByRole('button', { name: 'Start' }).click();
 
